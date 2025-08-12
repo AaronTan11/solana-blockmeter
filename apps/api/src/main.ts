f835ded683ad8617ec1ddc4bf17dc480274d25e4
AaronTan11 <tanweihup@gmail.com>
2025-08-12 16:52:01 +0800

    fix: update CORS configuration to allow all origins
    - Changed the CORS settings in main.ts to allow requests from any origin by setting the origin to '*'. This simplifies the configuration by removing specific origins previously listed.
    
    - there is issues with aws configs, so doing this temporarily

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,12 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'http://43.216.59.45',
-      'https://tanweihup.dev',
-      'http://tanweihup.dev'
-    ],
+    origin: '*',
     methods: ['GET'],
   });
   await app.listen(process.env.PORT ?? 3000);
