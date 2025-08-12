0809d3f943c13ae2f4f5d77f9393eedbd428cdb5
AaronTan11 <tanweihup@gmail.com>
2025-08-12 16:50:00 +0800

    fix: update CORS configuration to allow multiple origins
    - Modified the CORS settings in main.ts to support multiple origins, including localhost, a specific IP address, and two domain names.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'http://43.216.59.45',
+      'https://tanweihup.dev',
+      'http://tanweihup.dev'
+    ],
     methods: ['GET'],
   });
   await app.listen(process.env.PORT ?? 3000);
